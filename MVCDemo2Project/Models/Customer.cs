64af89b924f673182dca18710b2ec6a0c031f867
Arif.Mosharraf <Arif.Mosharraf@T3-10-D163>
2018-04-30 16:28:55 -0500

    Entity Framework

---
+++ MVCDemo2Project/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCDemo2Project.Models
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
---
+++ MVCDemo2Project/Models/CustomerInformationSystemsContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVCDemo2Project.Models
+{
+    public partial class CustomerInformationSystemsContext : DbContext
+    {
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CustomerInformationSystems;Trusted_Connection=True;");
+        //            }
+        //        }
+
+        public CustomerInformationSystemsContext(DbContextOptions<CustomerInformationSystemsContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.CustomerEmail)
+                    .HasColumnName("customerEmail")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CustomerName)
+                    .HasColumnName("customerName")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
